ce584555ec08916fc5a120c69cdd7a73c75c8de3
sameeeeer <lastking.me01@gmail.com>
2019-12-10 20:44:35 +0545

    Login layout

---
+++ app/src/main/java/com/example/blogging/Users/LoginSignup.java
@@ -0,0 +1,16 @@
+package com.example.blogging.Users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.blogging.R;
+
+public class LoginSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup);
+    }
+}
