1510cc6e8c6180b7d5971d540b76faa3d1f6a320
sameeeeer <lastking.me01@gmail.com>
2020-02-07 19:44:08 +0545

    Login Ui testing done

--- app/src/main/java/com/example/blogging/RetrofitHelper/Helper.java
+++ app/src/main/java/com/example/blogging/RetrofitHelper/Helper.java
@@ -14,12 +14,11 @@ public class Helper {
 
     private static Retrofit retrofit;
 
-//    private static final String BASE_URL = "http://127.0.0.1:3000/";
-//    public static final String IMAGE_URL = "http://10.0.2.2:3000/image/";
-//
-    private static final String BASE_URL = "http://10.0.2.2:3000/";
+    private static final String BASE_URL = "http://127.0.0.1:3000/";
     public static final String IMAGE_URL = "http://10.0.2.2:3000/image/";
-public static final  String Token = "Bearer ";
+//    private static final String BASE_URL = "http://10.0.2.2:3000/";
+//    public static final String IMAGE_URL = "http://10.0.2.2:3000/image/";
+public static String Token = "Bearer ";
 
     public static Retrofit getInstance() {
 
--- app/src/test/java/com/example/blogging/UnitTests.java
+++ app/src/test/java/com/example/blogging/UnitTests.java
@@ -1,42 +1,65 @@
 package com.example.blogging;
 
+import android.telecom.Call;
 import android.util.Log;
 
+import com.example.blogging.Bbl.PostBbl;
 import com.example.blogging.Bbl.Userbbl;
+import com.example.blogging.Fragment.UserProfileFragment;
 import com.example.blogging.Model.Usermodel;
+import com.example.blogging.Responseapi.UserResponse;
+import com.example.blogging.RetrofitHelper.Helper;
 
+import org.hamcrest.core.IsNot;
+import org.hamcrest.core.IsNull;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import javax.security.auth.login.LoginException;
+
+import retrofit2.Response;
+
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
 public class UnitTests {
     private Userbbl userbbl;
+    private PostBbl postBbl;
 
     @Before
     public void setup(){
         userbbl = new Userbbl();
     }
+    public void setPostBble(){postBbl = new PostBbl();}
 
     @Test
-    public void testLogin() {
+    public void testLogin_validDetails() {
         String email = "las@gmail.com";
         String password = "12345";
 
         Usermodel user = userbbl.login(email,password);
         Assert.assertNotNull(user);
     }
+    @Test
+    public void testLogin_InvalidDetails() {
+        String email = "kekkk@gmail.com";
+        String password = "12345";
 
+        Usermodel user = userbbl.login(email,password);
+        Assert.assertNull(user);
+    }
 
 
 
 
     }
+
+
